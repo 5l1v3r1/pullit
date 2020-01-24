b225acf4053796ba3f44b22bcfce710049af3c3f
Filtration <thefiltration@gmail.com>
2020-01-24 08:23:02 +0000

    update readme

--- modules/core/git.py
+++ modules/core/git.py
@@ -1,9 +1,11 @@
 import os
+from shutil import rmtree
 
 
 class Git:
 
     # Clone a git repo
+    # todo my]aybe use python
     @staticmethod
     def clone(name, url):
         os.system("git clone --single-branch --no-tags --depth 1 %s /tmp/pullit/git/%s > /dev/null 2>&1" % (url, name))
@@ -11,5 +13,8 @@ class Git:
     # Delete the repo
     @staticmethod
     def delete(name):
-        os.system("rm -rf /tmp/pullit/git/%s > /dev/null" % name)
-
+        try:
+            rmtree("/tmp/pullit/git/%s" % name)
+        except FileNotFoundError:
+            # I don't know why it would throw this but it does
+            return
